d4b0b1d0b3abf56742549f4257057a9675fc3d87
owolabijunior12 <owolabijunior12@gmail.com>
2025-01-13 18:58:33 +0100

    increase font sizes

--- components/quickStatistics.tsx
+++ components/quickStatistics.tsx
@@ -8,32 +8,32 @@ const QuickStatistics: React.FC = () => {
     <div className="bg-white rounded shadow p-2 md:p-4"> 
       <h2 className="text-xs font-semibold mb-2 ">Quick Statistics</h2>
       <div className="flex flex-row gap-2 justify-between items-center"> 
-        <div className="flex gap-1 items-center mb-2 md:mb-0">
+        <div className="flex gap-2 items-center mb-2 md:mb-0">
           <div className="rounded-full text-sm bg-slate-100 p-1 border">
             🏆
           </div>
           <div className="flex flex-col items-start">
-            <span className="text-xs font-bold">{score?.rank || 0}</span>
+            <span className="text-sm md:text-lg  font-bold">{score?.rank || 0}</span>
             <span className="text-gray-300 md:uppercase text-[6px] md:text-sm">Your Rank</span>
           </div>
         </div>
         <div className="w-px h-7 bg-black"></div> 
-        <div className="flex gap-1 items-center mb-2 md:mb-0">
+        <div className="flex gap-2 items-center mb-2 md:mb-0">
           <div className="rounded-full text-sm bg-slate-100 p-1 border">
             🗓️
           </div>
           <div className="flex flex-col items-start">
-            <span className="text-xs font-bold">{score?.percentile || 0}%</span>
+            <span className="text-sm md:text-lg  font-bold">{score?.percentile || 0}%</span>
             <span className="text-gray-300 md:uppercase text-[6px] md:text-sm">Percentage</span>
           </div>
         </div>
         <div className="w-px h-7 bg-black"></div> 
-        <div className="flex gap-1 items-center mb-2 md:mb-0">
+        <div className="flex gap-2 items-center mb-2 md:mb-0">
           <div className="rounded-full bg-slate-100 p-1 text-sm border">
             ✅
           </div>
           <div className="flex flex-col items-start">
-            <span className="text-xs font-bold">{score?.currentScore || 0}/15</span>
+            <span className="text-sm md:text-lg font-bold">{score?.currentScore || 0}/15</span>
             <span className="text-gray-300 md:uppercase text-[6px] md:text-sm">Correct Answer</span>
           </div>
         </div>
